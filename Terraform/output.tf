a9a31151a35e7c675500c82abe42ecf254e50848
Selçuk Karadağ <r.selcukkaradag@gmail.com>
2022-10-11 19:18:56 +0200

    add new outputs

--- Terraform/output.tf
+++ Terraform/output.tf
@@ -1,8 +1,17 @@
+output "app_name" {
+  value = "${var.app_name}"
+}
 output "queue_URL" {
   value = aws_sqs_queue.queue.id
 }
 output "test_cURL" {
   value = "curl -X POST -H 'Content-Type: application/json' -d '{\"id\":\"selcuk\", \"file\":\"file\"}' ${aws_api_gateway_deployment.producer.invoke_url}/"
+}
+output "apigateway_url" {
+  value = "${aws_api_gateway_deployment.producer.invoke_url}"
+}
+output "ecr_url" {
+  value = "${aws_ecr_repository.jumbo.repository_url}"
 }
 output "lb_url" {
   description = "URL of load balancer"
