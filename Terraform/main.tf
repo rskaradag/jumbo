74d347b43accb40c96c7c8d0dfab355e5b4ad4b6
Selçuk Karadağ <r.selcukkaradag@gmail.com>
2022-10-11 18:21:51 +0200

    fmt

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -20,8 +20,8 @@ terraform {
 }
 
 provider "aws" {
-  region                  = var.aws_region
-  access_key              = var.AWS_ACCESS_KEY_ID
-  secret_key              = var.AWS_SECRET_ACCESS_KEY
+  region     = var.aws_region
+  access_key = var.AWS_ACCESS_KEY_ID
+  secret_key = var.AWS_SECRET_ACCESS_KEY
 }
 
