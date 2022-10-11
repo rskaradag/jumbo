270b497eef9c322cb69779cd13eed9816f17002e
Selçuk Karadağ <r.selcukkaradag@gmail.com>
2022-10-11 18:16:39 +0200

    remove unneccesary lines

--- Terraform/lambda/consumer.py
+++ Terraform/lambda/consumer.py
@@ -1,21 +1,15 @@
 import json
 import os
-from botocore.session import Session
-from botocore.config import Config
+import boto3
 
-s = Session()
-sqs = s.create_client('sqs', config=Config(connect_timeout=5, read_timeout=60, retries={'max_attempts': 2}))
+sqs = boto3.client('sqs')
 
 def lambda_handler(event, context):
-    print(event)
-    #messages=sqs.receive_message(QueueUrl=os.environ["QUEUE_URL"],MaxNumberOfMessages=10, WaitTimeSeconds=20, MessageAttributeNames=['All'])
-    #print(messages)
     for item in event["Records"]:
         body=json.loads(item["body"])
         os.system("echo 'SQSFILE' >> " + os.environ["EFS_PATH"] + "/" + body["id"] + "-" + body["file"])
         print("file is created - " + os.environ["EFS_PATH"] + "/" + body["id"] + "-" + body["file"])
-         
-        print(item["receiptHandle"])
+
         delete_message(item["receiptHandle"])
 
     return {
@@ -23,7 +17,5 @@ def lambda_handler(event, context):
         }
 
 def delete_message(receiptHandle):
-    print('delete_receipt - ' + str(receiptHandle))
-    print(os.environ["QUEUE_URL"])
     delete= sqs.delete_message(QueueUrl=os.environ["QUEUE_URL"], ReceiptHandle=receiptHandle) 
     print('message is deleted - ' + str(delete))
