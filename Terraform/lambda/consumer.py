9930a51fc2828b5ef596c977543d405ebafb8854
Selçuk Karadağ <r.selcukkaradag@gmail.com>
2022-10-12 02:06:47 +0200

    update

--- Terraform/lambda/consumer.py
+++ Terraform/lambda/consumer.py
@@ -9,7 +9,7 @@ def lambda_handler(event, context):
         body=json.loads(item["body"])
         os.system("echo 'SQSFILE' >> " + os.environ["EFS_PATH"] + "/" + body["id"] + "-" + body["file"])
         print("file is created - " + os.environ["EFS_PATH"] + "/" + body["id"] + "-" + body["file"])
-
+        
         delete_message(item["receiptHandle"])
 
     return {
