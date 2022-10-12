5e037ccc212c400aafc9d0a348f2367e0ff25dcd
Selçuk Karadağ <r.selcukkaradag@gmail.com>
2022-10-12 03:19:37 +0200

    merged files

--- Terraform/vpc.tf
+++ Terraform/vpc.tf
@@ -24,3 +24,133 @@ resource "aws_vpc_endpoint" "sqs" {
   ]
 
 }
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.jumbo_vpc.id
+  tags = {
+    Name        = "${var.app_name}-igw"
+    Environment = var.app_environment
+  }
+
+}
+
+resource "aws_subnet" "private" {
+  vpc_id            = aws_vpc.jumbo_vpc.id
+  count             = length(var.private_subnets)
+  cidr_block        = element(var.private_subnets, count.index)
+  availability_zone = element(var.availability_zones, count.index)
+
+  tags = {
+    Name        = "${var.app_name}-private-subnet-${count.index + 1}"
+    Environment = var.app_environment
+  }
+}
+
+resource "aws_subnet" "public" {
+  vpc_id            = aws_vpc.jumbo_vpc.id
+  count             = length(var.public_subnets)
+  cidr_block        = element(var.public_subnets, count.index)
+  availability_zone = element(var.availability_zones, count.index)
+
+  tags = {
+    Name        = "${var.app_name}-public-subnet-${count.index + 1}"
+    Environment = var.app_environment
+  }
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.jumbo_vpc.id
+
+  tags = {
+    Name        = "${var.app_name}-routing-table-public"
+    Environment = var.app_environment
+  }
+}
+
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.jumbo_vpc.id
+
+  tags = {
+    Name        = "${var.app_name}-routing-table-private"
+    Environment = var.app_environment
+  }
+}
+
+resource "aws_route" "public" {
+  route_table_id         = aws_route_table.public.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.igw.id
+}
+
+resource "aws_route" "private" {
+  route_table_id         = aws_route_table.private.id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = aws_nat_gateway.nat_gw.id
+}
+
+resource "aws_route_table_association" "public" {
+  count          = length(var.public_subnets)
+  subnet_id      = element(aws_subnet.public.*.id, count.index)
+  route_table_id = aws_route_table.public.id
+}
+resource "aws_route_table_association" "private" {
+  count          = length(var.private_subnets)
+  subnet_id      = element(aws_subnet.private.*.id, count.index)
+  route_table_id = aws_route_table.private.id
+}
+
+resource "aws_eip" "nat_eIP" {
+  vpc = true
+}
+
+resource "aws_nat_gateway" "nat_gw" {
+  allocation_id = aws_eip.nat_eIP.id
+  subnet_id     = aws_subnet.public[0].id
+}
+
+resource "aws_security_group" "efs-sg" {
+  name        = "efs_security_group"
+  description = "Allow SSH and HTTP"
+  vpc_id      = aws_vpc.jumbo_vpc.id
+
+  ingress {
+    description = "SSH from VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "HTTPS from VPC"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "EFS mount target"
+    from_port   = 2049
+    to_port     = 2049
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "HTTP from VPC"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 2049
+    to_port     = 2049
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
